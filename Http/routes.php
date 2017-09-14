84e3c9cd25a2fb9786de134480351a766dae553c
Shashi Kant Singh <shashi@foodtalkindia.com>
2017-09-14 12:11:23 +0530

    check user session in user profile by athuprivilage middleware

--- Http/routes.php
+++ Http/routes.php
@@ -72,7 +72,7 @@ $app->get('/', function() use ($app) {
 		
 		$app->get('cuisine', [ 'uses' =>'RestaurantController@cuisine']);
 
-		$app->get('profile', [ 'uses' =>'UserController@profile']);
+		$app->get('profile', [ 'middleware' => 'athuprivilage', 'uses' =>'UserController@profile']);
 		
 // 		$app->group(['namespace' => 'App\Http\Controllers\Privilege'],function($app){
 			
