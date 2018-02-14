787e07e9ec787d3247eadd7b597d6ce3934de3b3
Malde Chavda <chavdamaldep@gmail.com>
2018-02-14 17:27:19 +0530

    added coupon info in appfeed

--- Http/Controllers/Privilege/AnalyticsController.php
+++ Http/Controllers/Privilege/AnalyticsController.php
@@ -17,7 +17,7 @@ use App\Models\Privilege\OfferRedeemed;
 use Carbon\Carbon;
 
 class AnalyticsController extends Controller {
-	
+
 	public function transactions(Request $request){
 
 		if(isset($_GET['from'])){
@@ -25,14 +25,14 @@ class AnalyticsController extends Controller {
 			if (isset($_GET['to'])){
 				$s1 = ' and paytm_order_status.created_at > "'.$_GET['from'].'" and paytm_order_status.created_at < "'.$_GET['to'].'" ';
 				$s2 = ' and exp_purchases.created_at > "'.$_GET['from'].'" and exp_purchases.created_at < "'.$_GET['to'].'" ';
-				
-			}else 
+
+			}else
 			{
 				$s1 = ' and paytm_order_status.created_at > "'.$_GET['from'].'" ';
 				$s2 = ' and exp_purchases.created_at > "'.$_GET['from'].'" ';
 			}
 		}else {
-			
+
 			$s1 = ' and paytm_order_status.created_at >= DATE(NOW()) - INTERVAL 7 DAY ';
 			$s2 = ' and exp_purchases.created_at >= DATE(NOW()) - INTERVAL 7 DAY ';
 		}
@@ -49,20 +49,20 @@ class AnalyticsController extends Controller {
 		INNER JOIN user on user.id = exp_purchases_order.user_id
 		INNER JOIN experiences on exp_purchases_order.exp_id = experiences.id  
 		WHERE exp_purchases.payment_status = "TXN_SUCCESS" '.$s2;
-	 	
+
 	 	$result = DB::connection('ft_privilege')->select( DB::raw($sql) );
-	 	
+
 	 	return $this->sendResponse ( $result, self::SUCCESS_OK_NO_CONTENT);
 	}
-	
-	
+
+
 	public function users(Request $request ,$days = 7){
-		
-		
+
+
 		$overall = DB::connection('ft_privilege')->select( DB::raw('select IF( subscription.id is null, "unpaid",  IF( subscription_type_id = 3,  "trial", "paid") ) as status, count(1) as count  from user LEFT JOIN subscription on subscription.user_id = user.id  GROUP by status'));
-		
+
 // 		$lastdays = DB::connection('ft_privilege')->select( DB::raw('select IF( subscription.id is null, "unpaid", "paid" ) as status, count(1) as count  from user LEFT JOIN subscription on subscription.user_id = user.id where user.created_at >= DATE(NOW()) - INTERVAL '.$days.' DAY GROUP by status'));
-		
+
 		DB::connection('ft_privilege')->statement('SET @i=0');
 		$total = DB::connection('ft_privilege')->select( DB::raw('SELECT theday as date, IFNULL(cnt, 0 ) as count FROM (SELECT DATE(ADDDATE(DATE_SUB(NOW(), INTERVAL '.$days.' DAY), INTERVAL @i:=@i+1 DAY)) AS theday FROM cuisine HAVING @i < DATEDIFF(now(), DATE_SUB(NOW(), INTERVAL '.$days.' DAY))) as calander left join (select count(1) cnt, DATE_FORMAT(created_at,"%Y-%m-%d") as onbord from user GROUP by onbord) as u on u.onbord = calander.theday'));
 
@@ -71,15 +71,15 @@ class AnalyticsController extends Controller {
 
 		DB::connection('ft_privilege')->statement('SET @i=0');
 		$trial = DB::connection('ft_privilege')->select( DB::raw('SELECT theday as date, IFNULL(cnt, 0 ) as count FROM (SELECT DATE(ADDDATE(DATE_SUB(NOW(), INTERVAL '.$days.' DAY), INTERVAL @i:=@i+1 DAY)) AS theday FROM cuisine HAVING @i < DATEDIFF(now(), DATE_SUB(NOW(), INTERVAL '.$days.' DAY))) as calander left join (select count(1) cnt, DATE_FORMAT(subscription.created_at,"%Y-%m-%d") as onbord from user LEFT JOIN subscription on subscription.user_id = user.id WHERE subscription.subscription_type_id = "3" GROUP by onbord ) as u on u.onbord = calander.theday'));
-		
+
 		DB::connection('ft_privilege')->statement('SET @i=0');
 		$unpaid = DB::connection('ft_privilege')->select( DB::raw('SELECT theday as date, IFNULL(cnt, 0 ) as count FROM (SELECT DATE(ADDDATE(DATE_SUB(NOW(), INTERVAL '.$days.' DAY), INTERVAL @i:=@i+1 DAY)) AS theday FROM cuisine HAVING @i < DATEDIFF(now(), DATE_SUB(NOW(), INTERVAL '.$days.' DAY))) as calander left join (select count(1) cnt, DATE_FORMAT(user.created_at,"%Y-%m-%d") as onbord from user LEFT JOIN subscription on subscription.user_id = user.id WHERE subscription.id is null GROUP by onbord ) as u on u.onbord = calander.theday'));
-		
+
 		$cnt = 0;
 		foreach ($overall as $val){
 			$cnt = $cnt + $val->count;
 		}
-		
+
 		array_unshift($overall, array(
 				'status'=>'all',
 				'count' => (string) $cnt
@@ -97,100 +97,108 @@ class AnalyticsController extends Controller {
 		);
 		return $this->sendResponse ( $result );
 	}
-	
+
 	public function offers(Request $request ,$days = 7){
-		
+
 // 		$overall = DB::connection('ft_privilege')->select( DB::raw(''));
 
 		$overall = DB::connection('ft_privilege')->select( DB::raw('SELECT count(offer_id) count, offer.title ,offer.id FROM offer left JOIN `offer_redeemed` on offer.id = offer_id group by offer.id'));
-		
+
 		$result = array(
 				'overall'=>$overall
 		);
 		$offers = Offer::where('is_disabled', '0')->get();
-		
+
 		foreach ($offers as $offer){
 			DB::connection('ft_privilege')->statement('SET @i=0');
 			$result['datewise'][$offer->id] = DB::connection('ft_privilege')->select( DB::raw('SELECT theday as date, IFNULL(cnt, 0 ) as count FROM (SELECT DATE(ADDDATE(DATE_SUB(NOW(), INTERVAL '.$days.' DAY), INTERVAL @i:=@i+1 DAY)) AS theday FROM cuisine HAVING @i < DATEDIFF(now(), DATE_SUB(NOW(), INTERVAL '.$days.' DAY))) as calander left join (select count(1) cnt, DATE_FORMAT(offer_redeemed.created_at,"%Y-%m-%d") as redeemed_on from offer_redeemed left JOIN `offer` on offer.id = offer_id WHERE offer.id = '.$offer->id.' GROUP by redeemed_on) as o on o.redeemed_on = calander.theday'));
 		}
 		return $this->sendResponse ( $result );
 	}
-	
+
 
 	public function restaurants(Request $request ,$top = 3, $days = 30){
-		
+
 		$result=array();
-		
+
 		$result['restaurants'] = DB::connection('ft_privilege')->select( DB::raw('SELECT COUNT(1) count FROM `restaurant` WHERE is_disabled = 0'));
-		
+
 		$result['outlet'] = DB::connection('ft_privilege')->select( DB::raw('SELECT COUNT(1) as count FROM `outlet` , restaurant WHERE restaurant.id = outlet.resturant_id and restaurant.is_disabled = 0 AND outlet.is_disabled=0'));
 
 		$result['top'] = DB::connection('ft_privilege')->select( DB::raw('SELECT count(1) count , outlet.id, outlet.name FROM `outlet` , offer_redeemed WHERE outlet.id = offer_redeemed.outlet_id GROUP BY outlet.id  
 ORDER BY `count`  DESC LIMIT '.$top));
-		
+
 		$result['latest_top'] = DB::connection('ft_privilege')->select( DB::raw('SELECT count(1) count , outlet.id, outlet.name FROM `outlet` , offer_redeemed WHERE outlet.id = offer_redeemed.outlet_id AND offer_redeemed.created_at >= DATE(NOW()) - INTERVAL '.$days.' DAY GROUP BY outlet.id ORDER BY `count`  DESC LIMIT '.$top));
-		
+
 		return $this->sendResponse ( $result );
 	}
-	
-	
+
+
 	// gets a user with id
 	public function get(Request $request, $entity, $id = false) {
-		
+
 		$where['entity_table'] = $entity;
-		
+
 		if($id)
 			$where['entity_id'] = $id;
-		
+
 		$result =  DBLog::where($where)
-		->paginate(DBLog::PAGE_SIZE);		
-		
+		->paginate(DBLog::PAGE_SIZE);
+
 		return $this->sendResponse ( $result );
 	}
-	
+
 
 	public function top_users(Request $request ,$top = 5, $days = 30){
 
 		$result=array();
 		$result['top'] = DB::connection('ft_privilege')->select( DB::raw('SELECT count(1) count , user.id, user.name FROM `user` , offer_redeemed WHERE user.id = offer_redeemed.user_id GROUP BY user.id ORDER BY `count`  DESC LIMIT '.$top));
 		$result['latest_top'] = DB::connection('ft_privilege')->select( DB::raw('SELECT count(1) count , user.id, user.name FROM `user` , offer_redeemed WHERE user.id = offer_redeemed.user_id AND offer_redeemed.created_at >= DATE(NOW()) - INTERVAL '.$days.' DAY GROUP BY user.id ORDER BY `count`  DESC LIMIT '.$top));
-		
+
 		return $this->sendResponse ( $result );
-		
+
 	}
-	
-	
+
+
 	public function signups(Request $request) {
-		
+
 		$query = user::select('user.id', 'name', 'phone', 'user.email', 'gender', DB::raw('IF(expiry is null ,"unpaid",  IF( subscription_type_id = 3,  "trial", "paid")) as status'), 'user.created_at as signup_on')
 		->leftjoin('subscription', 'user.id', '=','subscription.user_id')
 // 		->whereNull('subscription.expiry')
 		->where('user.is_verified', '=', '1')
 		->orderBy('user.created_at', 'desc');
-		
+
 		if(isset($_GET['after']))
 			$query->where('user.created_at', '>', $_GET['after']);
-			
+
 			$result = $query->paginate(OfferRedeemed::PAGE_SIZE);
-			
+
 			return $this->sendResponse ( $result );
 	}
-	
-	
-	public function purchases(Request $request) {
-		
-		$query = user::select('user.id', 'name', 'phone', 'user.email', 'gender', DB::raw('IF(expiry is null ,"unpaid", "paid") as status'), 'subscription.created_at as purchased_on')
-		->join('subscription', 'user.id', '=','subscription.user_id')
-		->where('subscription.subscription_type_id', '=', '1')
-		->orderBy('subscription.created_at', 'desc');
-		
-		if(isset($_GET['after']))
-			$query->where('subscription.created_at', '>', $_GET['after']);
-			
-			$result = $query->paginate(OfferRedeemed::PAGE_SIZE);
-			
-			return $this->sendResponse ( $result );
-	}
+
+
+    public function purchases(Request $request)
+    {
+        $query = PaytmOrderStatus::select('user.id', 'user.name', 'user.phone', 'user.email', 'gender', DB::raw('IF(expiry is null ,"unpaid", "paid") as status'), DB::raw('IF(coupons.code is null, "NULL", coupons.code) as coupon'), 'subscription.created_at as purchased_on')
+            ->join('paytm_order', 'paytm_order.id', '=', 'paytm_order_status.paytm_order_id')
+            ->leftJoin('coupons', 'coupons.id', '=', 'coupon_id')
+            ->join('user', 'user.id', '=', 'paytm_order.user_id')
+            ->join('subscription', 'user.id', '=','subscription.user_id')
+            ->where('paytm_order_status.payment_status', 'TXN_SUCCESS')
+            ->where('subscription.subscription_type_id', '=', '1')
+            ->orderBy('subscription.created_at', 'desc');
+
+        /*$query = user::select('user.id', 'name', 'phone', 'user.email', 'gender', DB::raw('IF(expiry is null ,"unpaid", "paid") as status'), 'subscription.created_at as purchased_on')
+        ->join('subscription', 'user.id', '=','subscription.user_id')
+        ->where('subscription.subscription_type_id', '=', '1')
+        ->orderBy('subscription.created_at', 'desc');*/
+
+        if (isset($_GET['after'])) {
+            $query->where('subscription.created_at', '>', $_GET['after']);
+        }
+
+        return $this->sendResponse($query->paginate(OfferRedeemed::PAGE_SIZE));
+    }
 
 	public function eventPurchases(Request $request)
 	{
@@ -207,18 +215,18 @@ ORDER BY `count`  DESC LIMIT '.$top));
 	}
 
 	public function redeemptions(Request $request) {
-		
+
 		$query = OfferRedeemed::select('offer_redeemed.id as redeemed_id', 'offers_redeemed', 'offer_redeemed.created_at as redeemed_on', 'user_id', 'user.name as user_name', 'offer_id', 'offer.title', 'outlet_id', 'outlet.name as outlet_name', 'area' )
 	 	->join('user', 'user.id', '=','offer_redeemed.user_id' )
 	 	->join('offer', 'offer.id', '=','offer_redeemed.offer_id' )
 	 	->join('outlet', 'outlet.id', '=','offer_redeemed.outlet_id' )
 	 	->orderBy('offer_redeemed.created_at', 'desc');
-		
+
 		if(isset($_GET['after']))
 			$query->where('offer_redeemed.created_at', '>', $_GET['after']);
-	 	
+
 	 	$result = $query->paginate(OfferRedeemed::PAGE_SIZE);
-			
+
 		return $this->sendResponse ( $result );
 	}
 
