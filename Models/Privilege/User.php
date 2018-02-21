7d194561573a94570f8ecbb7228d1226da2fefba
Malde Chavda <chavdamaldep@gmail.com>
2018-02-21 11:38:49 +0530

    working on user expire subscription

--- Models/Privilege/User.php
+++ Models/Privilege/User.php
@@ -37,11 +37,12 @@ class User extends BaseModel
     }
 
     /**
+     * @param $subscription
      * @return bool
      */
     public function isSubscriptionExpired($subscription)
     {
-        if ($subscription && $subscription->subscription_type_id == 3) {
+        if ($subscription->first() && $subscription->first()->subscription_type_id == 3) {
             return ! Carbon::parse($subscription->expiry) >= Carbon::today();
         }
 
